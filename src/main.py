66ee42a61102263bc346ecc145d9a0bb5def6306
AhmedRaisi <ahmedalraisi@gmail.com>
2023-12-27 17:28:37 +0400

    Added multiple request feature

--- src/main.py
+++ src/main.py
@@ -4,18 +4,37 @@ import nlp_processor
 import rpa_tasks
 
 def main():
-    # Greet the user and ask for their query
-    customer_query = input("Hello, how can I assist you today? ")
+    print("Hello, I am an automated customer bot. How can I assist you today?")
 
-    # Process the query using NLP
-    processed_query = nlp_processor.preprocess_text(customer_query)
-    intent = nlp_processor.recognize_intent(processed_query)
+    context = {
+        "username": "JohnDoe",  # Default username
+        "order_id": "unknown",  # Default order_id
+        "reset_link": "http://example.com/reset",
+        "refund_form_link": "http://example.com/refund",
+        "tracking_link": "http://example.com/track"
+        # Add other default context variables as needed
+    }
 
-    # Generate automated response based on the intent
-    response = rpa_tasks.generate_response(intent)
+    while True:
+        customer_query = input()
 
-    # Print the response
-    print(response)
+        # Update context based on customer_query if needed
+
+        # Process the query using NLP
+        processed_query = nlp_processor.preprocess_text(customer_query)
+        intent = nlp_processor.recognize_intent(processed_query)
+
+        # Generate automated response based on the intent
+        response = rpa_tasks.generate_response(intent, context)
+
+        # Print the response
+        print(response)
+
+        # Ask if more help is needed instead of satisfaction check
+        follow_up = input("Is there anything else I can help you with? (yes/no): ").strip().lower()
+        if follow_up != "yes":
+            print("Thank you for contacting us. Have a great day!")
+            break
 
 if __name__ == "__main__":
     main()
